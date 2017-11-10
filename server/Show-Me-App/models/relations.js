00de8f5b61bd1499f39cce7d8e329155559091a7
appukhairnar92@gmail.com <Appu@Apoorvs-MacBook-Pro.local>
2017-11-10 02:16:39 -0700

    Added a new model for relations.

---
+++ server/Show-Me-App/models/relations.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var PaperModel = require('./papers.js');
+var UserModel = require('./users.js');
+var Schema = mongoose.Schema;
+
+var commentsSchema = new Schema({
+	comment : {type: String, required:true},
+	commentBy : {type: Schema.ObjectId, ref:'UserModel'}
+});
+
+var CommentModel = mongoose.model('CommentModel', commentsSchema);
+
+var relationsSchema = new Schema({
+	relationFrom : {type: Schema.ObjectId, ref:'PaperModel'},
+	relationTo : {type: Schema.ObjectId, ref:'PaperModel'},
+	upvotes : [{type: Schema.ObjectId, ref:'UserModel'}],
+	downvotes : [{type: Schema.ObjectId, ref:'UserModel'}],
+	comments : [{type: Schema.ObjectId, ref:'CommentModel'}]
+});
+
+relationsSchema.virtual('name').get(function(){
+	return this.relationFrom + ' to ' + relationTo;
+});
+
+relationsSchema.virtual('url').get(function(){
+	return '/catalog/relation/' + this._id;
+});
+
+module.exports = mongoose.model('DomainModel', relationsSchema);
