2db35f8fffd4cbbd492890267251ba6a478f124a
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2017-11-14 01:48:38 -0700

    Angular 4 project inital structure. Static Website.

---
+++ FrontEndDevelopment/show-me/src/app/research-areas/research-areas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-areas',
+  templateUrl: './research-areas.component.html',
+  styleUrls: ['./research-areas.component.css']
+})
+export class ResearchAreasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
