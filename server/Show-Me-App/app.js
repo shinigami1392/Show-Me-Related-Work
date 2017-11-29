663c177b3590533a3c3f08d84e8ea96dbcff8dea
appukhairnar92@gmail.com <Appu@apoorvs-mbp.mobile.asu.edu>
2017-11-28 18:14:08 -0700

    Added cors

--- server/Show-Me-App/app.js
+++ server/Show-Me-App/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,6 +13,7 @@ var relations = require('./routes/relations');
 var graphNode = require('./routes/graphNode');
 
 var app = express();
+app.use(cors());
 
 var models = require('./models/models');
 models.createModels();
