0529530fc2faf833703796af8f680a7e000ab964
adutta14 <adutta14@asu.edu>
2018-02-15 16:16:18 -0700

    Removed app.closeserver()

--- server/Show-Me-App/spec/findUserspec.js
+++ server/Show-Me-App/spec/findUserspec.js
@@ -1,6 +1,6 @@
 var request = require("request");
-var app = require("../app.js")
-var base_url = "http://localhost:8081/"
+
+var base_url = "http://localhost:8081/";
 
 describe(" checks showME Server is running or not", function() {
   describe("GET /", function() {
@@ -14,16 +14,16 @@ describe(" checks showME Server is running or not", function() {
     });
 	
 	
-	//test to see if user details are fetched or not from the mongoDB
+	//test to see if user2 details are fetched or not from the mongoDB
     it("returns user details", function(done) {
 	  var user_url = base_url + "users/userId/user2"
       request.get(user_url, function(error, response, body) {
 		var body_res = JSON.parse(body);
-		expect(body_res.found).toBe(false)
-		expect(body_res.user.first_name).toBe("First4");
-		expect(body_res.user.last_name).toBe("Last4");
+		expect(body_res.found).toBe(true);
+		expect(body_res.user.first_name).toBe("First2");
+		expect(body_res.user.last_name).toBe("Last2");
 		expect(body_res.user.email).toBe("akhairna@asu.edu");
-		app.closeServer();
+		//app.closeServer();
 		done();
       });
     });
