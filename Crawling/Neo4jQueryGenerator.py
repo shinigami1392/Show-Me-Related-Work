72928b60b21a02cd694b9433b2428ddc5f1b66d2
shimigami1392 <pushkar.ladhe@asu.edu>
2018-02-15 23:17:47 -0700

    Task #180 script for generating neo4j query out of crawled data successfull developed.

---
+++ Crawling/Neo4jQueryGenerator.py
@@ -0,0 +1,42 @@
+import json as json
+
+json_data = json.load(open('response.json'))
+json_obj = json_data[0]
+
+query_set = ""
+relation_query_set = "CREATE\n"
+
+for json_obj in json_data:
+    query = "CREATE (Paper"
+    query = query + json_obj["id"]+":ResearchPaper {Title:'"
+    query = query + json_obj["title"] + "', Author: '"
+    author_list = json_obj["authors"]
+
+
+    for i in range(0, author_list.__len__()-1):
+        query = query + author_list[i] + ","
+
+    query = query + author_list[author_list.__len__()-1] + "', "
+    query = query + "Domain: 'Mechanical',"
+    query = query + "Link: '" + json_obj["link"] + "'})\n"
+    query_set = query_set + query
+
+#print query_set
+ref_count = 0;
+for json_obj in json_data:
+    references = json_obj["references"]
+    ref_count = ref_count + references.__len__()
+    query = ""
+    for reference in references:
+        query = query + "(Paper" + json_obj["id"]+")-[:HAS_REFERRED]->(Paper"+reference+"),\n"
+    relation_query_set = relation_query_set + query
+
+#print ref_count
+
+new = list(relation_query_set)
+new[new.__len__()-2] = ";"
+new = ''.join(new)
+#print new
+
+file = open("query.txt","w")
+file.write(query_set+"\n\n"+new)
