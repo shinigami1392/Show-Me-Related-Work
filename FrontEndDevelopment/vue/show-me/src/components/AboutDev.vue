0c1bac709193af2578ab64ed13355c17e48f032a
Rajat Sinha <rsinha13@asu.edu>
2018-02-22 00:29:53 -0700

    UI and allignment improvements

--- FrontEndDevelopment/vue/show-me/src/components/AboutDev.vue
+++ FrontEndDevelopment/vue/show-me/src/components/AboutDev.vue
@@ -2,41 +2,46 @@
   <div id="aboutsection" class="aboutdev aboutdev-default customaboutdev">
     <div class="aboutdevheader">
       <div id="devSection">
-        <table style="width:100%">
+        <table style="width:80%;" align="center">
         <tr>
         <td>
           <div id=dev1>
-          	<h3 style="margin:0;">Software developer</h3>
+          	<p style=" font-size:20px; margin:0;">Software developer</p>
             <p style="margin-bottom:10; margin-top:0;">Apoorv Khairnar</p>
               <p style="float: left; margin:0;"><img src="https://media-exp2.licdn.com/media/p/5/005/05b/1c4/15227f6.jpg" height="120px" width="130px" border="1px"></p>
-              <p>I am a graduate student at Arizona State University and currently pursuing my Master's in Software Engineering who is proficient in containerization of linux tools / softwares, web server management, web applications development, efficient server implementation, effective handling of client-server communication, REST APIs development, database management and graphics pipeline, openGL, agile methods. I am a computer science engineer who is mathematics expert and technology adroit. A very influential leader and a motivating teacher with excellent communication skills with clients, team members and students. An organized and self-motivated person who works well under pressure, always keeps a positive environment in team and has supportive attitude to deliver product under deadline.</p>
+              <p style="padding-left:140px;">I am a graduate student at Arizona State University and currently pursuing my Master's in Software Engineering who is proficient in containerization of linux tools / softwares, web server management, web applications development, efficient server implementation, effective handling of client-server communication, REST APIs development, database management and graphics pipeline, openGL, agile methods. I am a computer science engineer who is mathematics expert and technology adroit. A very influential leader and a motivating teacher with excellent communication skills with clients, team members and students. An organized and self-motivated person who works well under pressure, always keeps a positive environment in team and has supportive attitude to deliver product under deadline.</p>
           </div>
         </td>
         </tr>
 
+        <hr>
 
         <tr>
         <td>
           <div id=dev1>
-          	<h3 style="margin:0;">Software developer</h3>
+          	<p style=" font-size:20px; margin:0;">Software developer</p>
             <p style="margin-bottom:10; margin-top:0;">Pushkar Ladhe</p>
               <p style="float: left; margin:0;"><img src="https://media-exp2.licdn.com/media/AAMABADuAAgAAQAAAAAAABFRAAAAJDFmOWQwZmIxLTQ5NDQtNDY0OC1hMjRlLTY5OTEzYjA1M2QzZg.bin" height="120px" width="130px" border="1px"></p>
-              <p>A graduate student in Software Engineering with 2+ years of professional experience in developing robust applications using Java, Spring, Hibernate, REST and Microservices. Also, having advanced knowledge and hands-on experience in distributed computing using Hadoop, Spark, MapReduce, and Kafka. Worked on JavaScript frameworks like Node.js, Express.js, vue.js, and jQuery.</p>
+              <p style="padding-left:140px;">A graduate student in Software Engineering with 2+ years of professional experience in developing robust applications using Java, Spring, Hibernate, REST and Microservices. Also, having advanced knowledge and hands-on experience in distributed computing using Hadoop, Spark, MapReduce, and Kafka. Worked on JavaScript frameworks like Node.js, Express.js, vue.js, and jQuery.</p>
           </div>
         </td>
         </tr>
 
+        <hr>
 
         <tr>
         <td>
           <div id=dev2>
-          	<h3 style="margin:0;">Software developer</h3>
+          	<p style=" font-size:20px; margin:0;">Software developer</p>
             <p style="margin-bottom:10; margin-top:0;">Rajat Sinha</p>
               <p style="float: left; margin:0;"><img src="https://webapp4.asu.edu/photo-ws/directory_photo/rsinha13?blankImage=false&size=medium" height="120px" width="130px" border="1px"></p>
-              <p>Determined and self-motivated software engineering graduate student with 3 years of work experience in the field of Computer Vision – software development actively seeking full-time job opportunities.</p>
+              <p style="padding-left:140px;">Determined and self-motivated software engineering graduate student with 3 years of work experience in the field of Computer Vision – software development actively seeking full-time job opportunities.</p>
           </div>
         </td>
         </tr>
+
+        <hr>
+        
         </table>
       </div>
     </div>
@@ -51,13 +56,10 @@
   display: flex;
   align-items: center;
   justify-content: space-around;\
-  margin-top: 200px;
+  margin-top: 50px;
 }
 .aboutdevheader{
       border-radius: 3px;
-      width: 100%;
-      height: 650px;
-      background-color: #343a40;
     }
 
 .devSection {
