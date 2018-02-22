5b34b679ec76806576bfd20318f6495154875fc7
Apoooorv <akhairna@asu.edu>
2018-02-22 14:38:47 -0700

    Created a function which can be callable from outside

--- Crawling/Neo4jQueryGenerator.py
+++ Crawling/Neo4jQueryGenerator.py
@@ -1,43 +1,42 @@
 import json as json
+import os
 
-json_data = json.load(open('response.json'))
-json_obj = json_data[0]
+def loadData(papers):
+    json_data = papers
+    query_set = ""
+    relation_query_set = "CREATE "
 
-query_set = ""
-relation_query_set = "CREATE "
+    for json_obj in json_data:
+        query = "CREATE (Paper"
+        query = query + json_obj["id"]+":ResearchPaper {Title:'"
+        query = query + json_obj["title"] + "', Author: '"
+        author_list = json_obj["authors"]
 
-for json_obj in json_data:
-    query = "CREATE (Paper"
-    query = query + json_obj["id"]+":ResearchPaper {Title:'"
-    query = query + json_obj["title"] + "', Author: '"
-    author_list = json_obj["authors"]
+        for i in range(0, author_list.__len__()-1):
+            query = query + author_list[i] + ","
+
+        query = query + author_list[author_list.__len__()-1] + "', "
+        query = query + "Domain: '"+json_obj["stream"] + "',"
+        query = query + "Link: '" + json_obj["link"] + "'})\n"
+        query_set = query_set + query
+
+    ref_count = 0;
+    for json_obj in json_data:
+        references = json_obj["references"]
+        ref_count = ref_count + references.__len__()
+        query = ""
+        for reference in references:
+            query = query + "(Paper" + json_obj["id"]+")-[:HAS_REFERRED]->(Paper"+reference+"),"
+        relation_query_set = relation_query_set + query
+
+    new = list(relation_query_set)
+    new[new.__len__()-1] = ";"
+    new = ''.join(new)
+    print new
 
 
-    for i in range(0, author_list.__len__()-1):
-        query = query + author_list[i] + ","
-
-    query = query + author_list[author_list.__len__()-1] + "', "
-    query = query + "Domain: 'Mechanical',"
-    query = query + "Link: '" + json_obj["link"] + "'})\n"
-    query_set = query_set + query
-
-#print query_set
-ref_count = 0;
-for json_obj in json_data:
-    references = json_obj["references"]
-    ref_count = ref_count + references.__len__()
-    query = ""
-    for reference in references:
-        query = query + "(Paper" + json_obj["id"]+")-[:HAS_REFERRED]->(Paper"+reference+"),"
-    relation_query_set = relation_query_set + query
-
-#print ref_count
-
-new = list(relation_query_set)
-new[new.__len__()-1] = ";"
-new = ''.join(new)
-print new
-#print new
-
-file = open("query.txt","w")
-file.write(query_set+"\n"+new)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filepath = os.path.join(dir_path, 'NodeQueryRunner')
+    filepath = os.path.join(filepath, 'query.txt')
+    file = open(filepath,"wb")
+    file.write(query_set+"\n"+new)
