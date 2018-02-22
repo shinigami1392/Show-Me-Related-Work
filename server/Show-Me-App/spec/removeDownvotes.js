02f46873c594c4eb8e7fe25a1bbf87d55e86a0ad
adutta14 <adutta14@asu.edu>
2018-02-21 18:56:07 -0700

    Task 167 - Test cases for removeDownvotes API - Commit 1

---
+++ server/Show-Me-App/spec/removeDownvotes.js
@@ -0,0 +1,38 @@
+var request = require("request");
+var app = require("../app.js");
+var base_url = "http://localhost:8081/relations/downvote/remove?";
+
+describe(" SHowMe Relations", function() {
+  describe("GET /", function() {
+	  
+	//test to check if invalid Relation Id is properly handled by the application.
+    it("returns status code 200", function(done) {
+	  var relation_url = base_url + "id=678&user=user0";
+      request.get(relation_url, function(error, response, body) {
+        expect(response.statusCode).not.toBe(200);
+        done();
+      });
+    });
+	
+	//test to check if invalid user Id is properly handled by the application.
+    it("returns status code 200", function(done) {
+	  var user_url = base_url + "id=13&user=user1000";
+      request.get(user_url, function(error, response, body) {
+        expect(response.statusCode).not.toBe(200);
+        done();
+      });
+    });
+	  
+	//test to see if downvote is properly removed.
+    it("updates the downvote for a user for a particular relation", function(done) {
+	  var rel_url = base_url + "id=13&user=user0";
+      request.get(rel_url, function(error, response, body) {
+		var body_res = JSON.parse(body);
+		console.log(body_res.body_res);
+		expect(body_res.updated).toBe(true);			
+		done();
+      });
+    });
+  });
+});
+
