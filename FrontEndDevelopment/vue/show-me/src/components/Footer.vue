84d4117bec5dc79d8c26e2a8ea2ee2019538f461
Rajat Sinha <rsinha13@asu.edu>
2018-02-27 01:57:38 -0700

    #190 - Footer completed. The logo still needs to be fixed.

--- FrontEndDevelopment/vue/show-me/src/components/Footer.vue
+++ FrontEndDevelopment/vue/show-me/src/components/Footer.vue
@@ -1,21 +1,45 @@
 <template>
   <div id="footer" class="footer footer-default customfooter">
     <div class="footerheader">
-      <div id="footerSection">
-        <table style=" margin-top: 1%;">
-          <tr>
-            <td>
-              <p style="padding-left:140px; color:#666970;"><b>2018</b></p>
-            </td>
-            <td>
-              <p style="color:white;"><b>ShowMe</b></p>
-            </td>
-            <td >
-              <img style="padding-left:10px;" src="" alt="ShowMe Logo" height="130px" width="130px" border="1px">
-            </td>
-          </tr>
-        </table>
-      </div>
+
+        <div id="footerSection">
+
+          <div style="clear: left;">
+            <p style="float: left;">
+              <table style=" margin-top: 15%;">
+                <tr>
+                  <td>
+                    <p style="padding-left:140px; color:#666970;"><b>2018</b></p>
+                  </td>
+                  <td>
+                    <p style="color:white;"><b>ShowMe</b></p>
+                  </td>
+                </tr>
+              </table>
+            </p>
+
+            <p style="padding-left:400px; margin-top: 10px; float: left;">
+              <img src="https://static.wixstatic.com/media/db5142_5e2a3676c94348c7b271a382dec59ee8~mv2.png/v1/fill/w_310,h_305,al_c,usm_0.66_1.00_0.01/db5142_5e2a3676c94348c7b271a382dec59ee8~mv2.png" alt="ShowMe Logo" height="130px" width="130px" border="1px">
+            </p>
+
+            <p style="padding-left:400px; margin-top: 50px; float: left; color:white;">
+              <i class="fab fa-facebook-f fa-lg"></i>
+            </p>
+            <p style="padding-left:430px; margin-top: -38px; float: left; color:white;">
+              <i class="fab fa-twitter fa-lg"></i>
+            </p>
+            <p style="padding-left:470px; margin-top: -38px; float: left; color:white;">
+              <i class="fas fa-envelope fa-lg"></i>
+            </p>
+            <p style="padding-left:510px; margin-top: -40px; float: left; color:white;">
+              <i class="fas fa-phone-square fa-lg"></i>
+            </p>
+
+        </div>
+
+        </div>
+
+
     </div>
 
   </div>
