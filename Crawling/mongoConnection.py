4a0b7d325af4f015d08af801269e29133a778f38
Apoooorv <akhairna@asu.edu>
2018-02-26 19:48:12 -0700

    Saving data to domain models

--- Crawling/mongoConnection.py
+++ Crawling/mongoConnection.py
@@ -6,6 +6,7 @@ class mongoClient:
 		self.port = "27017"
 		self.client = None
 		self.dbName = "showMe"
+		self.totalDomains = 0
 
 	def formMongoURL(self):
 		return "mongodb://" + self.host + ":" + self.port
@@ -18,11 +19,30 @@ class mongoClient:
 		db = self.client[self.dbName]
 		result = db.domainmodels.insert_one(domain)
 
-
-	def savePapers(self, objects):
+	def savePapers(self, objects, stream):
 		db = self.client[self.dbName]
 		print 'saving papers', len(objects)
 		errors = []
+		document = db.domainmodels.find_one({'domainName':stream})
+		if document:
+			print 'inserting objects'
+			try:
+				papers = document['papers']
+				papers += objects
+				db.domainmodels.update_one(
+					{"id":document['id']}, 
+					{"$set":{"papers":papers}
+					})
+			except Exception, e:
+				print e.message
+			
+		else:
+			print 'creating documents'
+			db.domainmodels.insert_one({
+				'id': self.totalDomains,
+				'domainName': stream,
+				'papers': objects
+				})
 		for paper in objects:
 			try:
 				result = db.papermodels.insert_one(paper)
