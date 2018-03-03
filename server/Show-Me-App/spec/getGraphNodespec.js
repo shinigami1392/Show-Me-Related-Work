639018ed3ffd05ffbc169367e74da7e8a18337fc
adutta14 <adutta14@asu.edu>
2018-03-03 00:56:44 -0700

    Task 254 - Write test cases for getGraphNode API - Final Commit

--- server/Show-Me-App/spec/getGraphNodespec.js
+++ server/Show-Me-App/spec/getGraphNodespec.js
@@ -21,26 +21,49 @@ describe(" checks showME Server is running or not", function() {
 		expect(body_res.name).toBe("An overview of Machine Learning");
 		expect(body_res.author).toBe("Corbonell J.G, Michalski R.S, Mitchell T.M");
 		expect(body_res.url).toBe("https://link.springer.com/chapter/10.1007/978-3-662-12405-5_1");
-		expect(body_res.id).toBe("23");
-		expect(body_res.id).toBe("23");
 		done();
 	  });
 	});
 	
-	//test to check the inbound connections of the currently fetched node.
+	//test to check all the inbound connections of the currently fetched node.
     it("returns the inbound connections of a graph node", function(done) {
 	    var user_url = base_url + "graphNode/graphNode/7";
 	    request.get(user_url, function(error, response, body){
 		var body_res = JSON.parse(body);
+		expect(body_res.incoming_relations.length).toBe(2);
 		expect(body_res.incoming_relations[0].id).toBe("7");
 		expect(body_res.incoming_relations[0].source_name).toBe("Learning boolean concepts in the presence of many irrelevant features");
-		expect(body_res.incoming_relations[0].source_id).toBe("2");		
+		expect(body_res.incoming_relations[0].source_id).toBe("2");	
+		expect(body_res.incoming_relations[0].weight).toBe("0");		
 		expect(body_res.incoming_relations[1].id).toBe("8");
 		expect(body_res.incoming_relations[1].source_name).toBe("A study of instance-based algorithms for supervised learning tasks: mathematical, empirical and psychological evaluations");
-		expect(body_res.incoming_relations[1].source_id).toBe("10");	
+		expect(body_res.incoming_relations[1].source_id).toBe("10");
+		expect(body_res.incoming_relations[1].weight).toBe("0");		
 		done();
 	  });
 	});
+	
+	//test to check all the outbound connections of the currently fetched node.
+    it("returns the outbound connections of a graph node", function(done) {
+	    var user_url = base_url + "graphNode/graphNode/12";
+	    request.get(user_url, function(error, response, body){
+		var body_res = JSON.parse(body);
+		expect(body_res.outgoing_relations.length).toBe(3);
+		expect(body_res.outgoing_relations_relations[0].id).toBe("21");
+		expect(body_res.outgoing_relations_relations[0].destination_name).toBe("Knowledge Acquisition Via Incremental Conceptual Clustering");
+		expect(body_res.outgoing_relations_relations[0].destination_id).toBe("20");	
+		expect(body_res.outgoing_relations[0].weight).toBe("0");
+		expect(body_res.outgoing_relations_relations[1].id).toBe("22");
+		expect(body_res.outgoing_relations_relations[1].destination_name).toBe("Acquiring a Noun Classification from Predicate-Argument Structures");
+		expect(body_res.outgoing_relations_relations[1].destination_id).toBe("21");
+		expect(body_res.outgoing_relations[0].weight).toBe("0");
+		expect(body_res.outgoing_relations_relations[2].id).toBe("20");
+		expect(body_res.outgoing_relations_relations[2].destination_name).toBe("Learning Efficient Classification Procedures and Their Application to Chess End Games");
+		expect(body_res.outgoing_relations_relations[2].destination_id).toBe("19");
+		expect(body_res.outgoing_relations[0].weight).toBe("0");
+		done();
+	  });
+	});	
   });
 });
 
