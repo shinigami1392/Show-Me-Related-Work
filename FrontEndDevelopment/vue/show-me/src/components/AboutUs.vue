6ee2ce57e4d548152d1e082ea140ba94826d27bf
pranjalsk <pkaranka@asu.edu>
2018-04-18 15:39:18 -0700

    task 378 fix the grid system

--- FrontEndDevelopment/vue/show-me/src/components/AboutUs.vue
+++ FrontEndDevelopment/vue/show-me/src/components/AboutUs.vue
@@ -31,192 +31,203 @@
     <h1>Meet The Team</h1>
 
     <div>
-        <div class="column">
-            <div class="card">
-              <img src="https://media.licdn.com/dms/image/C5603AQHVIKhwLSb-WA/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=tljseVkiOg1AqFfeLbm82EITJ1pgTjtoZU2h-ln81hM"
-                alt="Abhishek Dutta" style="width:100%">
-              <div class="container">
-                <h2>Abhishek Dutta</h2>
-                <p class="title">Backend Developer & Unit Test</p>
-                <p >
-                  I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-                </p>
-                <div class="email-icon">
-                  <i class="fas fa-envelope-square fa-lg"></i>
-                </div>
-                <p>adutta14@asu.edu</p>
-                <div class="linkedin-github">
-                  <a href="https://www.linkedin.com/in/abhishek-dutta-56347944/" target="_blank">
-                    <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-                  </a>
-                  <a href="https://github.com/adutta14" target="_blank">
-                    <i class="fab fa-github-square fa-2x"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
 
-
-      <div class="column">
-        <div class="card">
-          <img src="https://media.licdn.com/dms/image/C4E03AQHvKMW3611fCQ/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=vlEZcohnh97avodKRl-yjqjflKANqfvhoCwitOtTo5o"
-            alt="Apoorv Khairnar" style="width:100%">
-          <div class="container">
-            <h2>Apoorv Khairnar</h2>
-            <p class="title">Backend Developer- Crawler</p>
-            <p >
-                I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-              </p>
-            <div class="email-icon">
-              <i class="fas fa-envelope-square fa-lg"></i>
-            </div>
-            <p>akhairna@asu.edu</p>
-            <div class="linkedin-github">
-              <a href="https://www.linkedin.com/in/apoorv-khairnar/" target="_blank">
-                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-              </a>
-              <a href="https://github.com/Apoooorv" target="_blank">
-                <i class="fab fa-github-square fa-2x"></i>
-              </a>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div class="column">
-          <div class="card">
-            <img src="https://media.licdn.com/dms/image/C4E03AQE91deyaZT9bQ/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=xDxDrTEX78GkSNnl8mJx9Itu3OV5hZxu4zGQX_NFt2k"
-              alt="Chiraag Subramanian" style="width:100%">
             <div class="container">
-              <h2>Chiraag Subramanian</h2>
-              <p class="title">Fullstack Developer</p>
-              <p>
-                  I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-                </p>
-              <div class="email-icon">
-                <i class="fas fa-envelope-square fa-lg"></i>
-              </div>
-              <p>csubram2@asu.edu</p>
-              <div class="linkedin-github">
-                <a href="https://www.linkedin.com/in/chiraag-subramanian-6b839b71/" target="_blank">
-                  <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-                </a>
-                <a href="https://github.com/chiraag-subramanian" target="_blank">
-                  <i class="fab fa-github-square fa-2x"></i>
-                </a>
-              </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C5603AQHVnSzozphxAg/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=AGFtUPHrpUkg-PGYH3NO3o03YwRyRC9bS9DgGjC8qM4"
+                        alt="Pushkar Ladhe" style="width:100%">
+                    <div class="container">
+                        <h2>Pushkar Ladhe</h2>
+                        <p class="title">Backend Developer</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>pladhe@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/pladhe/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/shinigami1392" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+
+                <!--3-->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C5603AQH7iOKdxzyvsg/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=ra6R5PEL9ifwGmg8tfnqjDiRr8JoV09QTYqRomX2Fi4"
+                        alt="Pranjal Karankar" style="width:100%">
+                    <div class="container">
+                        <h2>Pranjal Karankar</h2>
+                        <p class="title">Frontend Developer</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>pkaranka@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/pranjalk20/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/pranjalsk" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                <!--4-->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C5603AQEPqWEh3C3siQ/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=Q_siM70vqztTVwlDLUFaj52RUX1VuB_wg2-pa0ULU3E"
+                        alt="Sachin Magar" style="width:100%">
+                    <div class="container">
+                        <h2>Sachin Magar</h2>
+                        <p class="title">Frontend Developer & Unit Test</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>smagar@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/sachin-magar/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/sachin-magar" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-      <div class="column">
-        <div class="card">
-          <img src="https://media.licdn.com/dms/image/C5603AQHVnSzozphxAg/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=AGFtUPHrpUkg-PGYH3NO3o03YwRyRC9bS9DgGjC8qM4"
-            alt="Pushkar Ladhe" style="width:100%">
-          <div class="container">
-            <h2>Pushkar Ladhe</h2>
-            <p class="title">Backend Developer</p>
-            <p >
-                I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-              </p>
-            <div class="email-icon">
-              <i class="fas fa-envelope-square fa-lg"></i>
+
+            <!--5-->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C4E03AQEz-hwy5edSBA/profile-displayphoto-shrink_200_200/0?e=1527487200&v=alpha&t=lp2eN47TtG9a6IRr_lzbkdc_-WKrfdy3qdJ4Q-5yzdY"
+                        alt="Rajat Sinha" style="width:100%">
+                    <div class="container">
+                        <h2>Rajat Sinha</h2>
+                        <p class="title">Frontend Developer</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>rsinha13@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/rajat-sinha/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/rajsjc" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p>pladhe@asu.edu</p>
-            <div class="linkedin-github">
-              <a href="https://www.linkedin.com/in/pladhe/" target="_blank">
-                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-              </a>
-              <a href="https://github.com/shinigami1392" target="_blank">
-                <i class="fab fa-github-square fa-2x"></i>
-              </a>
+
+                <!--5-->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C5603AQHVIKhwLSb-WA/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=tljseVkiOg1AqFfeLbm82EITJ1pgTjtoZU2h-ln81hM"
+                        alt="Abhishek Dutta" style="width:100%">
+                    <div class="container">
+                        <h2>Abhishek Dutta</h2>
+                        <p class="title">Backend Developer & Unit Test</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>adutta14@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/abhishek-dutta-56347944/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/adutta14" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <!--6-->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C4E03AQHvKMW3611fCQ/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=vlEZcohnh97avodKRl-yjqjflKANqfvhoCwitOtTo5o"
+                        alt="Apoorv Khairnar" style="width:100%">
+                    <div class="container">
+                        <h2>Apoorv Khairnar</h2>
+                        <p class="title">Backend Developer- Crawler</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>akhairna@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/apoorv-khairnar/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/Apoooorv" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!--7-->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://media.licdn.com/dms/image/C4E03AQE91deyaZT9bQ/profile-displayphoto-shrink_800_800/0?e=1527490800&v=alpha&t=xDxDrTEX78GkSNnl8mJx9Itu3OV5hZxu4zGQX_NFt2k"
+                        alt="Chiraag Subramanian" style="width:100%">
+                    <div class="container">
+                        <h2>Chiraag Subramanian</h2>
+                        <p class="title">Fullstack Developer</p>
+                        <p>
+                            I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
+                        </p>
+                        <div class="email-icon">
+                            <i class="fas fa-envelope-square fa-lg"></i>
+                        </div>
+                        <p>csubram2@asu.edu</p>
+                        <div class="linkedin-github">
+                            <a href="https://www.linkedin.com/in/chiraag-subramanian-6b839b71/" target="_blank">
+                                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                            <a href="https://github.com/chiraag-subramanian" target="_blank">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-      </div>
-
-
-      <div class="column">
-        <div class="card">
-          <img src="https://media.licdn.com/dms/image/C5603AQH7iOKdxzyvsg/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=ra6R5PEL9ifwGmg8tfnqjDiRr8JoV09QTYqRomX2Fi4"
-            alt="Pranjal Karankar" style="width:100%">
-          <div class="container">
-            <h2>Pranjal Karankar</h2>
-            <p class="title">Frontend Developer</p>
-            <p >
-                I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-              </p>
-            <div class="email-icon">
-              <i class="fas fa-envelope-square fa-lg"></i>
-            </div>
-            <p>pkaranka@asu.edu</p>
-            <div class="linkedin-github">
-              <a href="https://www.linkedin.com/in/pranjalk20/" target="_blank">
-                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-              </a>
-              <a href="https://github.com/pranjalsk" target="_blank">
-                <i class="fab fa-github-square fa-2x"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="column">
-        <div class="card">
-          <img src="https://media.licdn.com/dms/image/C5603AQEPqWEh3C3siQ/profile-displayphoto-shrink_800_800/0?e=1527487200&v=alpha&t=Q_siM70vqztTVwlDLUFaj52RUX1VuB_wg2-pa0ULU3E"
-            alt="Sachin Magar" style="width:100%">
-          <div class="container">
-            <h2>Sachin Magar</h2>
-            <p class="title">Frontend Developer & Unit Test</p>
-            <p >
-                I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-              </p>
-            <div class="email-icon">
-              <i class="fas fa-envelope-square fa-lg"></i>
-            </div>
-            <p>smagar@asu.edu</p>
-            <div class="linkedin-github">
-              <a href="https://www.linkedin.com/in/sachin-magar/" target="_blank">
-                <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-              </a>
-              <a href="https://github.com/sachin-magar" target="_blank">
-                <i class="fab fa-github-square fa-2x"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="column">
-          <div class="card">
-            <img src="https://media.licdn.com/dms/image/C4E03AQEz-hwy5edSBA/profile-displayphoto-shrink_200_200/0?e=1527487200&v=alpha&t=lp2eN47TtG9a6IRr_lzbkdc_-WKrfdy3qdJ4Q-5yzdY"
-              alt="Rajat Sinha" style="width:100%">
-            <div class="container">
-              <h2>Rajat Sinha</h2>
-              <p class="title">Frontend Developer</p>
-              <p >
-                  I have 4 years of work experience in Oracle. My area of interests are - AWS, Software security, and DevOps.
-                </p>
-              <div class="email-icon">
-                <i class="fas fa-envelope-square fa-lg"></i>
-              </div>
-              <p>rsinha13@asu.edu</p>
-              <div class="linkedin-github">
-                <a href="https://www.linkedin.com/in/rajat-sinha/" target="_blank">
-                  <i style="padding-right:10px;" class="fab fa-linkedin fa-2x"></i>
-                </a>
-                <a href="https://github.com/rajsjc" target="_blank">
-                  <i class="fab fa-github-square fa-2x"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
+    </div>
       
     </div>
 
