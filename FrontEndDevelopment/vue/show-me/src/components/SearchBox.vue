8d376af99b086651d4f291a07fa791934b3fd135
sachin-magar <smagar@asu.edu>
2018-04-17 22:30:56 -0700

    #377 : Removed CSS and added bootstrap libraries in searchBar.vue

--- FrontEndDevelopment/vue/show-me/src/components/SearchBox.vue
+++ FrontEndDevelopment/vue/show-me/src/components/SearchBox.vue
@@ -1,12 +1,10 @@
 <template>
-    <div id="custom-search-input">
-            <div >                    
-                <span class="input-group-btn">
-                    <input type="text" class="form-control input-lg" placeholder="What are you looking for..." />
-                    <button class="btn btn-info btn-lg fas fa-search" type="button"></button>
-                </span>
+        <div class="input-group" style="margin-right:20%; margin-left:60%; width:60%; margin-bottom:2%">
+             <input type="search" class="form-control " name="q" id="location" value="" maxlength="128" placeholder="Search Place..." autocomplete="off" />
+             <div class="input-group-addon catsubmit"><i class="fa fa-search"></i></div>
             </div>
-      </div>      
+          </div>
+     
 </template>
 
 
@@ -22,50 +20,5 @@
 
 
 <style>
-#custom-search-input{
-    background: -webkit-radial-gradient(circle, #fff, #fff);
-    margin-left: 500px;
-    margin-right: 500px;
-    align-items: center;
-    padding: 3px;
-    border: solid 2px #0E6390;
-    border-radius: 6px;
-    background-color: #fff;
-    margin-bottom: 5px;
-}
-
-#custom-search-input input{
-    width:250px;
-    border: 0;
-    box-shadow: none;
-    -webkit-transition: width 0.4s ease-in-out;
-    transition: width 0.4s ease-in-out;
-}
-
-#custom-search-input button{
-    margin: 2px 0 0 0;
-    background: none;
-    box-shadow: none;
-    color: #0E6390;
-    padding: 0 8px 0 10px;
-    border-left: solid 1px #ccc;    
-}
-
-#custom-search-input button:hover{
-    border: 0;
-    box-shadow: none;
-    border-left: solid 1px rgb(204, 204, 204);
-}
-
-@media only screen and (max-width: 1350px){
-    #custom-search-input input { width: 75%; }
-}
-
-
-@media only screen and (max-width: 1120px){
-    #custom-search-input input { width: 0px; }
-    #custom-search-input button { visibility: hidden; }
-}
-
 
 </style>
