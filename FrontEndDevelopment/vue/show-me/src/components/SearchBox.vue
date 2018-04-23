b68ab20e928cde45661edee34aeb7ebf95810af3
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2018-04-22 18:53:10 -0700

    [Task-344] Changed search box width and placeholder text.

--- FrontEndDevelopment/vue/show-me/src/components/SearchBox.vue
+++ FrontEndDevelopment/vue/show-me/src/components/SearchBox.vue
@@ -1,6 +1,6 @@
 <template>
-        <div class="input-group" style="margin-right:20%; margin-left:60%; width:60%; margin-bottom:2%">
-             <input type="search" class="form-control" v-model="search_text" name="q" id="location" value="" maxlength="128" placeholder="Search Place..." autocomplete="off" />
+        <div class="input-group" style="margin-right:20%; margin-left:60%; width:80%; margin-bottom:2%">
+             <input type="search" class="form-control" v-model="search_text" name="q" id="location" value="" maxlength="128" placeholder="Search Papers by Title, Author Names, Publication Year..." autocomplete="off" />
              <div class="input-group-addon searchbox" v-on:click="search"><i class="fa fa-search"></i></div>
             </div>
           </div>
